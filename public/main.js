95423bad1c9c69136c6b09c855ed0828eb40e9ea
mehdi havaei <havaei@imenaria.org>
2022-09-17 13:12:22 +0430

    fix: update class

--- public/main.js
+++ public/main.js
@@ -1,9 +1,12 @@
 "use strict";
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    //   readonly client: string;
+    //   private details: string;
+    //   public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  //   readonly client: string;
+  //   private details: string;
+  //   public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
