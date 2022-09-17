dfb755e77a40428f335883199879cb6bc9e94d7a
mehdi havaei <havaei@imenaria.org>
2022-09-17 13:01:47 +0430

    feat: add styles and some funcs

--- public/main.js
+++ public/main.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("hello world");
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invoices = [];
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.format());
+});
+// form
+const form = document.querySelector(".new-item-form");
+// inputs
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+// button
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
-console.log("hello world");
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invoices: Invoice[] = [];
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.format());
+});
+
+// form
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// inputs
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// button
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log(e);
+});
